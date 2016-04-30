9bad57a4535f62f11038300c40b5dc33178925d0
Emanuel Rabina <emanuelrabina@gmail.com>
2016-04-30 16:37:50 +1200

    Temp executor to run just the main example test file

---
+++ Tests/nz/net/ultraq/thymeleaf/tests/SingleLayoutTestExecutor.groovy
@@ -0,0 +1,53 @@
+/* 
+ * Copyright 2016, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.thymeleaf.tests
+
+import nz.net.ultraq.thymeleaf.LayoutDialect
+import nz.net.ultraq.thymeleaf.decorators.strategies.GroupingStrategy
+import nz.net.ultraq.thymeleaf.testing.JUnitTestExecutor
+
+import org.junit.runners.Parameterized
+import org.thymeleaf.dialect.IDialect
+import org.thymeleaf.standard.StandardDialect
+
+/**
+ * Temporary test class that executes just one of my test files so I can make
+ * sure that the Thymeleaf 3 migration is making some progress instead of being
+ * overwhelmed with test failures everywhere!
+ * 
+ * @author Emanuel Rabina
+ */
+class SingleLayoutTestExecutor extends JUnitTestExecutor {
+
+	final List<? extends IDialect> testDialects = [
+		new StandardDialect(),
+		new LayoutDialect(new GroupingStrategy())
+	]
+
+	/**
+	 * Return just the example Thymeleaf testing file.
+	 * 
+	 * @return List of just the example test file.
+	 */
+	@Parameterized.Parameters(name = '{0}')
+	static List<String> listGroupingLayoutDialectTests() {
+
+		return [
+		  'nz/net/ultraq/thymeleaf/tests/Decorator-FullLayout-FullContent.thtest'
+		]
+	}
+}
