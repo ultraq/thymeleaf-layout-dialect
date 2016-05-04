e42bc3b6dd2694373ec80404f3f7537d30c2dd87
Emanuel Rabina <emanuelrabina@gmail.com>
2016-05-04 14:09:02 +1200

    Split meta-programming extensions into own file

--- Source/nz/net/ultraq/thymeleaf/LayoutDialect.groovy
+++ Source/nz/net/ultraq/thymeleaf/LayoutDialect.groovy
@@ -23,10 +23,9 @@ import nz.net.ultraq.thymeleaf.decorators.strategies.AppendingStrategy
 import nz.net.ultraq.thymeleaf.fragments.FragmentProcessor
 import nz.net.ultraq.thymeleaf.includes.IncludeProcessor
 import nz.net.ultraq.thymeleaf.includes.ReplaceProcessor
+import nz.net.ultraq.thymeleaf.models.ModelExtensions
 
 import org.thymeleaf.dialect.AbstractProcessorDialect
-import org.thymeleaf.model.IModel
-import org.thymeleaf.model.IText
 import org.thymeleaf.processor.IProcessor
 import org.thymeleaf.standard.processor.StandardXmlNsTagProcessor
 import org.thymeleaf.templatemode.TemplateMode
@@ -43,178 +42,10 @@ class LayoutDialect extends AbstractProcessorDialect {
 	private static final int DIALECT_PRECEDENCE = 10
 
 	/**
-	 * Add the following methods to Thymeleaf's model objects.
+	 * Apply model extensions.
 	 */
 	static {
-
-		IModel.metaClass {
-
-			/**
-			 * Returns whether or not this event represents collapsible whitespace.
-			 * 
-			 * @return <tt>true</tt> if this is a collapsible text node.
-			 */
-			isWhitespaceNode << {
-				def thisEvent = delegate.get(0)
-				return thisEvent instanceof IText && thisEvent.whitespace
-			}
-		}
-
-		IText.metaClass {
-
-			/**
-			 * Returns whether or not this text event is collapsible whitespace.
-			 * 
-			 * @return <tt>true</tt> if, when trimmed, the text content is empty.
-			 */
-			isWhitespace << {
-				return delegate.text.trim().empty
-			}
-		}
-
-		//		Attribute.metaClass {
-//
-//			/**
-//			 * Returns whether or not an attribute is an attribute processor of
-//			 * the given name, checks both prefix:processor and
-//			 * data-prefix-processor variants.
-//			 * 
-//			 * @param prefix
-//			 * @param name
-//			 * @return <tt>true</tt> if this attribute is an attribute processor
-//			 *         of the matching name.
-//			 */
-//			equalsName << { String prefix, String name ->
-//				return delegate.originalName == "${prefix}:${name}" ?:
-//				       delegate.originalName == "data-${prefix}-${name}"
-//			}
-//		}
-//
-//		Element.metaClass {
-//
-//			/**
-//			 * Searches this and all children of this element for an element of
-//			 * the given name.
-//			 * 
-//			 * @param name
-//			 * @return The matching element, or <tt>null</tt> if no match was
-//			 *         found.
-//			 */
-//			findElement << { String name ->
-//				def search
-//				search = { element ->
-//					if (element.originalName == name) {
-//						return element
-//					}
-//					return element.elementChildren.find(search)
-//				}
-//				return search(delegate)
-//			}
-//
-//			/**
-//			 * Returns an attribute processor's value, checks both
-//			 * prefix:processor and data-prefix-processor variants.
-//			 * 
-//			 * @param prefix
-//			 * @param name
-//			 * @return The value of the matching processor, or <tt>null</tt> if
-//			 *         the processor doesn't exist on the element.
-//			 */
-//			getAttributeValue << { String prefix, String name ->
-//				return delegate.getAttributeValue("${prefix}:${name}") ?:
-//				       delegate.getAttributeValue("data-${prefix}-${name}")
-//			}
-//
-//			/**
-//			 * Returns whether or not the element has an attribute processor,
-//			 * checks both prefix:processor and data-prefix-processor variants.
-//			 * 
-//			 * @param prefix
-//			 * @param name
-//			 * @return <tt>true</tt> if the processor exists on the element.
-//			 */
-//			hasAttribute << { String prefix, String name ->
-//				return delegate.hasAttribute("${prefix}:${name}") ||
-//				       delegate.hasAttribute("data-${prefix}-${name}")
-//			}
-//
-//			/**
-//			 * Inserts a child node, creating a whitespace node before it so
-//			 * that it appears in line with all the existing children.
-//			 * 
-//			 * @param child Node to add.
-//			 * @param index Node position.
-//			 */
-//			insertChildWithWhitespace << { child, int index ->
-//				if (child) {
-//					def parent = delegate.parent
-//					def whitespace
-//					if (parent instanceof Document) {
-//						whitespace = new Text('\n\t')
-//					}
-//					else {
-//						def parentChildren = parent.children
-//						whitespace = new Text(parentChildren.get(parentChildren.indexOf(delegate) - 1).content + '\t')
-//					}
-//					delegate.insertChild(index, whitespace)
-//					delegate.insertChild(index + 1, child)
-//				}
-//			}
-//
-//			/**
-//			 * Removes an attribute processor from this element, checks both
-//			 * prefix:processor and data-prefix-processor variants.
-//			 * 
-//			 * @param prefix
-//			 * @param name
-//			 */
-//			removeAttribute << { String prefix, String name ->
-//				delegate.removeAttribute("${prefix}:${name}")
-//				delegate.removeAttribute("data-${prefix}-${name}")
-//			}
-//
-//			/**
-//			 * Removes a child node and the whitespace node immediately before
-//			 * so that the area doesn't appear too messy.
-//			 * 
-//			 * @param child Node to remove
-//			 */
-//			removeChildWithWhitespace << { child ->
-//				if (child) {
-//					def children = delegate.children
-//					def index = children.indexOf(child)
-//					delegate.removeChild(index)
-//					if (index > 0) {
-//						delegate.removeChild(index - 1)
-//					}
-//				}
-//			}
-//		}
-//
-//		Node.metaClass {
-//
-//			/**
-//			 * Returns whether or not this node represents collapsible
-//			 * whitespace.
-//			 *
-//			 * @return <tt>true</tt> if this is a collapsible text node.
-//			 */
-//			isWhitespaceNode << {
-//				return delegate instanceof Text && delegate.whitespace
-//			}
-//		}
-//
-//		Text.metaClass {
-//
-//			/**
-//			 * Returns whether or not this text node is collapsible whitespace.
-//			 *
-//			 * @return <tt>true</tt> if, when trimmed, the text content is empty.
-//			 */
-//			isWhitespace << {
-//				return delegate.content.trim().empty
-//			}
-//		}
+		ModelExtensions.apply()
 	}
 
 	private final SortingStrategy sortingStrategy
---
+++ Source/nz/net/ultraq/thymeleaf/models/ModelExtensions.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016, Emanuel Rabina (http://www.ultraq.net.nz/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.thymeleaf.models
+
+import org.thymeleaf.model.IModel
+import org.thymeleaf.model.IText
+
+/**
+ * Additional methods applied to the Thymeleaf model classes via Groovy
+ * meta-programming.
+ * 
+ * @author Emanuel Rabina
+ */
+class ModelExtensions {
+
+	/**
+	 * Applies several new methods to the Thymeleaf model classes.
+	 */
+	static void apply() {
+
+		IModel.metaClass {
+
+			/**
+			 * Return whether or not a model has a body, but checking if it has any
+			 * underlying events.
+			 * 
+			 * @return {@code true} if the model contains events.
+			 */
+			hasBody << {
+				return delegate.size() > 0
+			}
+
+			/**
+			 * Returns whether or not this event represents collapsible whitespace.
+			 * 
+			 * @return <tt>true</tt> if this is a collapsible text node.
+			 */
+			isWhitespaceNode << {
+				def thisEvent = delegate.get(0)
+				return thisEvent instanceof IText && thisEvent.whitespace
+			}
+		}
+
+		IText.metaClass {
+
+			/**
+			 * Returns whether or not this text event is collapsible whitespace.
+			 * 
+			 * @return <tt>true</tt> if, when trimmed, the text content is empty.
+			 */
+			isWhitespace << {
+				return delegate.text.trim().empty
+			}
+		}
+	}
+}
