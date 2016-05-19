10be0f98d422b8abaa0ab4cda3f66acb903ff29a
Emanuel Rabina <emanuelrabina@gmail.com>
2016-05-19 21:35:30 +1200

    Confirm conditional comment merging already working

--- Tests/nz/net/ultraq/thymeleaf/tests/GradualLayoutTestExecutor.groovy
+++ Tests/nz/net/ultraq/thymeleaf/tests/GradualLayoutTestExecutor.groovy
@@ -46,6 +46,7 @@ class GradualLayoutTestExecutor extends JUnitTestExecutor {
 	static List<String> listGroupingLayoutDialectTests() {
 
 		return [
+			'nz/net/ultraq/thymeleaf/tests/decorators/html/Head-ConditionalComments.thtest',
 			'nz/net/ultraq/thymeleaf/tests/decorators/strategies/AppendingStrategy.thtest',
 			'nz/net/ultraq/thymeleaf/tests/decorators/xml/Document-XmlDecoration.thtest',
 			'nz/net/ultraq/thymeleaf/tests/decorators/Decorator-BodyOnlyLayout-BodyOnlyContent.thtest'
